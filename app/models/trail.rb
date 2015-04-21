36f469a5328763243153ec844f83d62f022d9a1d
Jenny Crawshaw <jennycrawshaw@gmail.com>
2015-04-21 18:55:38 +0000

    Refined search function

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -10,7 +10,7 @@ class Trail < ActiveRecord::Base
   after_validation :geocode
 
   def self.search(search)
-    where("state LIKE ? OR title LIKE ? OR address LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+    where("state LIKE ? OR title LIKE ? OR address LIKE ?", "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%")
   end
 
 end
